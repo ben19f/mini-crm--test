6f0a3371824fae5ae391038a8b1012ec627c86b9
ben <2390188@mail.ru>
2025-12-05 22:42:43 +0300

    Добавил функцию источника и источник

---
+++ add_source.py
@@ -0,0 +1,25 @@
+from database import SessionLocal
+from db_models import Source
+
+
+def add_source(db, source_name: str):
+    """
+    Добавляеm источник
+    """
+    source = Source(
+        source_name=source_name
+    )
+    db.add(source)
+    db.commit()
+
+
+
+db = SessionLocal()
+
+
+source = add_source(
+    db=db,
+    source_name="bot01"
+)
+
+db.close()

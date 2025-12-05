8f0877370fcd9d15ab9f34baf8633bf859eaf55e
ben <2390188@mail.ru>
2025-12-05 14:21:49 +0300

    создаю татаблиццу операторов

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#tt
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+DATABASE_URL = "sqlite:/home/ben/PycharmProjects/mini-crm--test/crm.db"
+
+
+
+#modeli
+Base = declarative_base()
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

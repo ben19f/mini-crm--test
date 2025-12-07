df6eabace4eb762aa2b4115faa958b58a299c6e6
ben <2390188@mail.ru>
2025-12-07 09:55:39 +0300

    тестим получение

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException
 
 from database import SessionLocal
 from pydantic import BaseModel
+from typing import List
+from db_models import Operator
 
+# app = FastAPI(title="crm API")
 
-app = FastAPI(title="crm API")
+db = SessionLocal()
 
 def get_db():
     """определяем БД"""
@@ -25,6 +28,21 @@ class OperatorResp(BaseModel):
         orm_mode = True
 
 
-@app.post("/operators/", response_model=OperatorResp, status_code=201)
-def create_operator(
 
+
+@app.post("/operators/", response_model=List(OperatorResp))
+def list_operators(active: Optional[bool] = None,
+        db):
+    """
+    ищем операторов список
+    """
+    query = db.query(Operator)
+    if active is not None:
+        query = query.filter(Operator.active_status == active)
+
+    operators = query.all()
+    return operators
+
+
+print(list_operators(active=None, db=db))
+db.close()

88c8efc393e569989bd0335435f9e3476833d266
ben <2390188@mail.ru>
2025-12-07 09:24:02 +0300

    вызвал БД

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from fastapi import FastAPI, HTTPException, Depends
+
+from database import SessionLocal
+
+app = FastAPI(title="crm API")
+
+def get_db():
+    """определяем БД"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
